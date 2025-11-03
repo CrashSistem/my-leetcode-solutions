9fbca713e1c2b57617a2a8f40c28593c8ccb8a5a
CrashSystem <vivoshilo2@gmail.com>
2025-11-03 05:26:51 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 2.1 MB (86.10%)

---
+++ my-solutions/0070-climbing-stairs/solution.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+    if n == 1 {return 1}    
+    if n == 2 {return 2}
+    let mut prev = 1;
+    let mut curr = 2;
+    for i in 3..=n{
+        let next = curr + prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr
+}
+
+}
