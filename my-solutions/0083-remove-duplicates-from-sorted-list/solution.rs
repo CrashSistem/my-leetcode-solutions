8cac4e95f288202762220e7c1b6839ed6bfb15b7
CrashSystem <vivoshilo2@gmail.com>
2025-11-04 03:00:04 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted List - Runtime - 0 ms (100.00%), Memory - 2.2 MB (16.33%)

---
+++ my-solutions/0083-remove-duplicates-from-sorted-list/solution.rs
@@ -0,0 +1,34 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut current = &mut head;
+
+    while let Some(node) = current {
+        while let Some(next) = &mut node.next {
+            if node.val == next.val {
+                node.next = next.next.take();
+            } else {
+                break;
+            }
+        }
+        current = &mut node.next;
+    }
+
+    return head;
+}
+}
