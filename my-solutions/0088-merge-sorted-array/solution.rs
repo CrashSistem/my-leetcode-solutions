f8f2ecf6bf36005871aeb3ed69b17f26a30244e2
CrashSystem <vivoshilo2@gmail.com>
2025-11-04 03:11:57 +0000

    Sync LeetCode submission - Merge Sorted Array - Runtime - 0 ms (100.00%), Memory - 2.2 MB (26.10%)

---
+++ my-solutions/0088-merge-sorted-array/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    let (mut m , mut n) = (m as usize, n as usize);
+    let mut pos = m + n;
+
+    while n > 0{
+        pos -= 1;
+        if m > 0 && nums1[m - 1] > nums2[n - 1] {
+            m -= 1;
+            nums1[pos] = nums1[m];
+        } else {
+            n -= 1;
+            nums1[pos] = nums2[n];
+        }
+    }
+}
+
+}
