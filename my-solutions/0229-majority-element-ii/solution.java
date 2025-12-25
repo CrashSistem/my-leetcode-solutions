72c15cb17a5f62e18cccf6d46fe2282a310ddf3e
CrashSystem <vivoshilo2@gmail.com>
2025-12-25 07:55:36 +0000

    Sync LeetCode submission - Majority Element II - Runtime - 2 ms (99.94%), Memory - 52.7 MB (29.09%)

---
+++ my-solutions/0229-majority-element-ii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+            int cand1 = 0, cand2 = 1, count1 = 0, count2 = 0;
+            int k = nums.length / 3;
+            for (int num : nums) {
+                if (num == cand1)  count1++;
+                else if (num == cand2)  count2++;
+                else if (count1 == 0) { cand1 = num; count1 = 1; }
+                else if (count2 == 0) { cand2 = num; count2 = 1; }
+                else {count1--;count2--;}
+            }
+            List<Integer> result = new ArrayList<>();
+            count1 = 0; count2 = 0;
+            for  (int num : nums) {
+                if (num == cand1) count1++;
+                else if (num == cand2) count2++;
+            }
+            if (count1 > k ) result.add(cand1);
+            if (count2 > k ) result.add(cand2);
+            return result;
+        }
+}
