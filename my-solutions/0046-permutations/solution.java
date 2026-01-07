27869be3eac9a06be03546088955a0499df207cc
CrashSystem <vivoshilo2@gmail.com>
2026-01-07 08:21:04 +0000

    Sync LeetCode submission - Permutations - Runtime - 2 ms (36.33%), Memory - 45.3 MB (90.30%)

---
+++ my-solutions/0046-permutations/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            boolean[] visited = new boolean[nums.length];
+            backtrack(result, new ArrayList<>(), nums, visited);
+            return result;
+
+        }
+        void backtrack (List<List<Integer>> result, List<Integer> tempList, int[] nums, boolean[] used) {
+            if (tempList.size() == nums.length) {
+                result.add(new ArrayList<>(tempList));
+                return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (used[i]) continue;
+
+                used[i] = true;
+                tempList.add(nums[i]);
+
+                backtrack(result, tempList, nums, used);
+
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+}
