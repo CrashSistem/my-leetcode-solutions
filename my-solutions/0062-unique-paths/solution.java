67c0120dbaf3f24205dc6e7d15843cd24648e159
CrashSystem <vivoshilo2@gmail.com>
2026-01-09 05:23:37 +0000

    Sync LeetCode submission - Unique Paths - Runtime - 0 ms (100.00%), Memory - 42.1 MB (65.58%)

--- my-solutions/0062-unique-paths/solution.java
+++ my-solutions/0062-unique-paths/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-            return helper(m, n, new int[m + 1][n + 1]);
-        }
-        int helper(int m, int n, int[][] arr) {
-            if (n < 1 || m < 1) return 0;
-            if (n == 1 || m == 1) return 1;
-            if (arr[m][n] != 0) return arr[m][n];
-            arr[m][n] = helper(m - 1, n, arr) +  helper(m, n - 1, arr);
-            return arr[m][n];
+            int[] dp = new int[n];
+            Arrays.fill(dp, 1);
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+            return dp[n - 1];
         }
 }
