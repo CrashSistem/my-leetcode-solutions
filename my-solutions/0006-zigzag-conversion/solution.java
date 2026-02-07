325b0ffc87e5f85d94eb78f2fd4799a76d9d5fa5
CrashSystem <vivoshilo2@gmail.com>
2026-02-07 04:48:10 +0000

    Sync LeetCode submission - Zigzag Conversion - Runtime - 4 ms (88.72%), Memory - 46.6 MB (50.57%)

---
+++ my-solutions/0006-zigzag-conversion/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String convert(String s, int numRows) {
+            if (numRows == 1 || s.length() <= numRows) return s;
+
+            StringBuilder[] sb = new StringBuilder[numRows];
+            for (int i = 0; i < numRows; i++) {
+                sb[i] = new StringBuilder();
+            }
+
+            int curRow = 0;
+            boolean goingDown = false;
+
+            for (char c : s.toCharArray()) {
+                sb[curRow].append(c);
+
+                if (curRow == numRows - 1 || curRow == 0) {
+                    goingDown = !goingDown;
+                }
+
+                curRow += goingDown ? 1 : -1;
+            }
+            StringBuilder res = new StringBuilder();
+            for (StringBuilder row : sb) {
+                res.append(row);
+            }
+            return res.toString();
+        }
+}
