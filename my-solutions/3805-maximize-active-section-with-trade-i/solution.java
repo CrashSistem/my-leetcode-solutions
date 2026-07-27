d3df0149f410fe70ed415b05e5a8e9d5b0f1f997
CrashSystem <vivoshilo2@gmail.com>
2026-07-27 11:57:55 +0000

    Sync LeetCode submission - Maximize Active Section with Trade I - Runtime - 47 ms (99.71%), Memory - 48 MB (45.05%)

--- my-solutions/3805-maximize-active-section-with-trade-i/solution.java
+++ my-solutions/3805-maximize-active-section-with-trade-i/solution.java
@@ -1,52 +1,32 @@
 class Solution {
 
-    private static class Block {
-
-        char type;
-        int length;
-
-        Block(char type, int length) {
-            this.type = type;
-            this.length = length;
-        }
-    }
-
     public int maxActiveSectionsAfterTrade(String s) {
+
+        // T: O(n)
+        // S: O(1)
+
         int totalOnes = 0;
-        int n = s.length();
-        List<Block> blocks = new ArrayList<>();
-        int i = 0;
+        int leftZeros = 0;
+        int curZeros = 0;
+        int maxGain = 0;
+        boolean hasBetween = false;
 
-        while (i < n) {
-            int start = i;
-            char ch = s.charAt(i);
-            while (i < n && ch == s.charAt(i)) {
-                if (ch == '1') totalOnes++;
-                i++;
-            }
-            blocks.add(new Block(ch, i - start));
-        }
+        for (char ch : s.toCharArray()) {
+            if (ch == '1') {
+                totalOnes++;
 
-        if (blocks.size() < 3) return totalOnes;
-        if (totalOnes == 0 || totalOnes == n) return totalOnes;
-
-        int maxOnes = totalOnes;
-
-        for (int j = 1; j < blocks.size() - 1; j++) {
-            Block current = blocks.get(j);
-            if (current.type == '1') {
-                Block left = blocks.get(j - 1);
-                Block right = blocks.get(j + 1);
-
-                if (left.type == '0' && right.type == '0') {
-                    maxOnes = Math.max(
-                            maxOnes,
-                            totalOnes + left.length + right.length
-                    );
+                if (curZeros > 0) {
+                    leftZeros = curZeros;
+                    curZeros = 0;
+                }
+                hasBetween = true;
+            } else { // '0'
+                curZeros++;
+                if (hasBetween && leftZeros > 0) {
+                    maxGain = Math.max(maxGain, leftZeros + curZeros);
                 }
             }
         }
-
-        return maxOnes;
+        return totalOnes + maxGain;
     }
 }
