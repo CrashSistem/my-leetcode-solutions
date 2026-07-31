98e63dbaf48d14cdcf53f9f3f64e3fb818646a3e
CrashSystem <vivoshilo2@gmail.com>
2026-07-31 09:36:34 +0000

    Sync LeetCode submission - Minimum Number of Pushes to Type Word II - Runtime - 9 ms (94.22%), Memory - 48.3 MB (18.85%)

---
+++ my-solutions/3276-minimum-number-of-pushes-to-type-word-ii/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < word.length(); i++) {
+            freq[word.charAt(i) - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int totalPushes = 0;
+        int count = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+
+            int pushMultiplier = (count / 8) + 1;
+            totalPushes += freq[i] * pushMultiplier;
+
+            count++;
+        }
+        return totalPushes;
+    }
+}
