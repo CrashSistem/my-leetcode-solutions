2976199d70f7840840163a579186c43222e419a4
CrashSystem <vivoshilo2@gmail.com>
2026-08-01 05:44:42 +0000

    Sync LeetCode submission - Predict the Winner - Runtime - 0 ms (100.00%), Memory - 42.8 MB (54.51%)

---
+++ my-solutions/0486-predict-the-winner/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] memo = new Integer[n][n];
+
+        return getScore(nums, 0, nums.length - 1, memo) >= 0;
+    }
+
+    int getScore(int[] nums, int left, int right, Integer[][] memo) {
+        if (left == right) return nums[left];
+        if (memo[left][right] != null) return memo[left][right];
+
+
+        int takeLeft = nums[left] - getScore(nums, left + 1, right, memo);
+        int takeRight = nums[right] - getScore(nums, left, right - 1, memo);
+        memo[left][right] = Math.max(takeLeft, takeRight);
+        return memo[left][right];
+    }
+}
