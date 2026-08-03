d7807892964fd003c802cea654632c37c614bd92
CrashSystem <vivoshilo2@gmail.com>
2026-08-03 14:06:12 +0000

    Sync LeetCode submission - Longest Common Prefix - Runtime - 0 ms (100.00%), Memory - 43.4 MB (32.51%)

--- my-solutions/0014-longest-common-prefix/solution.java
+++ my-solutions/0014-longest-common-prefix/solution.java
@@ -1,29 +1,13 @@
 class Solution {
-public String longestCommonPrefix(String[] strs) {
-    if (strs == null || strs.length == 0) return "";
+    public String longestCommonPrefix(String[] strs) {
+        String ans = strs[0];
 
-    int count = 0;
-    boolean stop = false;
-
-    // Проходим по всем буквам первого слова
-    for (int i = 0; i < strs[0].length() && !stop; i++) {
-        char currentChar = strs[0].charAt(i);
-
-        // Проверяем эту букву во всех остальных словах
-        for (int j = 1; j < strs.length; j++) {
-            // Если слово короче или буква не совпадает
-            if (i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
-                stop = true;
-                break;
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(ans)) {
+                ans = ans.substring(0, ans.length() - 1);
             }
         }
 
-        // Если все слова имеют эту букву на этой позиции
-        if (!stop) {
-            count++;
-        }
+        return ans;
     }
-
-    return strs[0].substring(0, count);
 }
-}   
